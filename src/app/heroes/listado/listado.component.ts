8f1400343f1e6993acef8dfcc497eb3b8f7312a5
Ignacio Del Solar <ignacio.delsolard@gmail.com>
2021-07-12 18:14:24 -0400

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Ironman','Hulk','Thor','Capitan America'];
+  heroeBorrado: string ='';
+  
+  borrarHeroe(){
+    const heroeBorrado = this.heroes.shift() || '';
+    this.heroeBorrado = heroeBorrado;
+  }
+
+}
